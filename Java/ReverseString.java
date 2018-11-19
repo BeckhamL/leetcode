895b95f14ed4bcd817993eb2c9157be02c5fc4ab
beckham <beckham.lam@mail.mcgill.ca>
2018-11-19 16:01:52 -0500

    reverse string my own implementation

--- Java/ReverseString.java
+++ Java/ReverseString.java
@@ -1,7 +1,33 @@
+// This is a solution using built in functions
 public class Solution {
     public String reverseString(String s) {
-
         return new StringBuilder(s).reverse().toString();
+    }
+}
 
+// My own Solution
+class Solution {
+    public String reverseString(String s) {
+
+        char[] charString = new char[s.length()];
+        int left = 0;
+        int right = s.length() - 1;
+
+        // create a char[] with every character in string
+        for (int i = 0; i < s.length(); i++) {
+            charString[i] = s.charAt(i);
+        }
+
+        // use 2 pointers and swap the chars until I reach the end
+        while (left < right) {
+            char temp = charString[left];
+            charString[left] = charString[right];
+            charString[right] = temp;
+            left++;
+            right--;
+        }
+
+        String newS = new String(charString);
+        return newS;
     }
 }
