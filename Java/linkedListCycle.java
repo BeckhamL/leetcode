648cddcd3721af764869da2e776fa23e8990b004
beckham <beckham.lam@mail.mcgill.ca>
2018-12-08 00:46:22 -0500

    linked list loop

---
+++ Java/linkedListCycle.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+
+        if (head.next == null) {
+            return false;
+        }
+
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+        while (slow != null && fast != null && fast.next != null) {
+            if (fast.val == slow.val) {
+                return true;
+            }
+            else {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+        }
+        return false;
+    }
+}
