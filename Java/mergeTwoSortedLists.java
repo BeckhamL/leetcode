d61c0e17b06cf1849ed219523b83f9da671f9bff
beckham <beckham.lam@mail.mcgill.ca>
2018-12-11 00:49:10 -0500

    merges 2 sorted - recur

--- Java/mergeTwoSortedLists.java
+++ Java/mergeTwoSortedLists.java
@@ -48,3 +48,27 @@ class Solution {
         return temp.next;
     }
 }
+
+// Recursive Solution
+// Little more difficult to understand
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
