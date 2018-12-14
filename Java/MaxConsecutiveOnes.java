10072d3e4a23d6e9065eb950cb4fd7e9b53843d4
beckham <beckham.lam@mail.mcgill.ca>
2018-12-14 00:36:57 -0500

    better solution for max consecutive ones

--- Java/MaxConsecutiveOnes.java
+++ Java/MaxConsecutiveOnes.java
@@ -1,26 +1,30 @@
+// Updated Solution
+// Loop through the list, if I see a '1' increment counter else keep track of max
+// Runtime: O(n)
+
 public class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
 
         int numberOfOnes = 0;
         int maxOnes = 0;
-        int currentLong = 0;
 
         for (int i = 0; i < nums.length; i++){
             if (nums[i] == 1) {
                 numberOfOnes++;
-                maxOnes = numberOfOnes;
             }
             if (nums[i] == 0){
-                currentLong = maxOnes;
+                if (numberOfOnes > maxOnes) {
+                    maxOnes = numberOfOnes;
+                }
                 numberOfOnes = 0;
             }
-            if (maxOnes > currentLong) {
-                maxOnes = maxOnes;
-            }
-            else {
-                maxOnes = currentLong;
-            }
         }
-        return maxOnes;
+
+        if (numberOfOnes > maxOnes) {
+            return numberOfOnes;
+        }
+        else {
+            return maxOnes;
+        }
     }
 }
