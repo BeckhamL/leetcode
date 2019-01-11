f978cc6e4f85598d2578f96fa1a6c7434449edc9
beckham <beckham.lam@mail.mcgill.ca>
2019-01-10 19:57:48 -0500

    valid anagram new solution

--- Java/validAnagram.java
+++ Java/validAnagram.java
@@ -29,3 +29,30 @@ class Solution {
         }
     }
 }
+
+// Solution: make both strings into char arrays and sort. Since they are anagrams they should contain the same values. Iterate and if they arent the same return false
+// Runtime: O(n log n)
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+
+        // O(n log n)
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        // O(n)
+        for (int i = 0; i < s1.length; i++) {
+            if (s1[i] != s2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
