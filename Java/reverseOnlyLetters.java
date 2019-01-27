6d22492f33d3ca239b3098c1ac559cdf50881840
beckham <beckham.lam@mail.mcgill.ca>
2019-01-27 01:18:17 -0500

    same problem

--- Java/reverseOnlyLetters.java
+++ Java/reverseOnlyLetters.java
@@ -1,6 +1,7 @@
 // Solution: Same idea as reversing a string. Check if character is a letter, if not move pointer if both chars are letters
 // swap them
 // Runtime: O(n)
+
 import java.util.*;
 
 class Solution {
@@ -25,6 +26,39 @@ class Solution {
                 chars[last] = temp;
                 first++;
                 last--;
+            }
+        }
+        return new String(chars);
+    }
+}
+
+// forgot I solved it. Intuition is the same as before.
+// Runtime: O(n)
+
+class Solution {
+    public String reverseOnlyLetters(String S) {
+
+        int first = 0;
+        int last = S.length() -1;
+
+        char[] chars =  S.toCharArray();
+
+        while (first < last) {
+            if (Character.isLetter(chars[first])) {
+                char temp = chars[first];
+                if (Character.isLetter(chars[last])) {
+
+                    chars[first] = chars[last];
+                    chars[last] = temp;
+                    first++;
+                    last--;
+                }
+                else {
+                    last--;
+                }
+            }
+            else {
+                first++;
             }
         }
         return new String(chars);
