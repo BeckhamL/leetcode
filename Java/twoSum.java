a4ad9ff5f294a06298d6809b77fe188271bae04f
beckham <beckham.lam@mail.mcgill.ca>
2019-01-29 00:27:50 -0500

    two sum

---
+++ Java/twoSum.java
@@ -0,0 +1,22 @@
+// Solution: Keep a hashmap of the complement of the target and index. If the complement isnt in the hashmap, add the current index and value. Else return the elements' complement
+// and index
+// Runtime: O(n)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(target-nums[i]) == null) {
+                map.put(nums[i], i);
+            }
+            else {
+                ans[0] = map.get(target-nums[i]);
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+}
