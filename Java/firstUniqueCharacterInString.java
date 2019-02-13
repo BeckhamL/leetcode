91bf0fa875345c4da245f70673f75e6d6b39c8e6
beckham <beckham.lam@mail.mcgill.ca>
2019-02-13 13:13:41 -0500

    first unique character in string fix

--- Java/firstUniqueCharacterInString.java
+++ Java/firstUniqueCharacterInString.java
@@ -19,7 +19,7 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             if (map.get(s.charAt(i)) == 1) {
-                return s.indexOf(s.charAt(i));
+                return i;
             }
         }
         return -1;
