f159e874474de79df4615149dd669335db41955b
beckham <beckham.lam@mail.mcgill.ca>
2019-02-13 12:49:45 -0500

    first unique character in string

---
+++ Java/firstUniqueCharacterInString.java
@@ -0,0 +1,27 @@
+// Solution: Have a hashmap store the character and number of occurences. The unique character should have only 1 as the value. Loop through string and whichever character has value 1 first, return import junit.framework.TestCase;
+// Runtime: O(n)
+
+class Solution {
+    public int firstUniqChar(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (map.containsKey(s.charAt(i))) {
+                int val = map.get(s.charAt(i));
+                map.put(s.charAt(i), val + 1);
+            }
+            else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return s.indexOf(s.charAt(i));
+            }
+        }
+        return -1;
+    }
+}
