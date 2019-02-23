f81a9b8a66373a9e73b6181f394713e072a0846f
beckham <beckham.lam@mail.mcgill.ca>
2019-02-22 21:23:32 -0500

    factorial

---
+++ Java/factorialIterRecur.java
@@ -0,0 +1,28 @@
+public class factorial{
+
+     public static void main(String []args){
+
+       int iter = factorialIter(4);
+       int recur = factorialRecur(4);
+
+       System.out.println(iter + " " + recur);
+     }
+
+     public static int factorialIter(int n) {
+
+         int val = 1;
+         for (int i = 1; i <= n;i++) {
+             val = val * i;
+         }
+         return val;
+     }
+
+     public static int factorialRecur(int n) {
+
+         if (n == 0) {
+             return 1;
+         }
+
+         return (n * factorialRecur(n-1));
+     }
+}
