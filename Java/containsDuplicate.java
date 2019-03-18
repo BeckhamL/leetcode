977b98faf1ea5309c19be5f58bd98974a7f37e37
beckham <beckham.lam@mail.mcgill.ca>
2019-03-17 21:09:00 -0400

    contains duplicate fix

--- Java/containsDuplicate.java
+++ Java/containsDuplicate.java
@@ -1,3 +1,6 @@
+// Solution: Sort the array, if 2 numbers next to each other the same return true; else return false.
+// Runtime: O(n log n)
+
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -15,5 +18,25 @@ class Solution {
             }
         }
         return contains;
+    }
+}
+
+// Solution: Use a hashmap to store values I have seen before, if hashmap contains the key return true; else false.
+// Runtime: O(n)
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return true;
+            }
+            else {
+                map.put(nums[i], 0);
+            }
+        }
+        return false;
     }
 }
