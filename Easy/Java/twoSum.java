969491955ee5d2408f29f6494d313e3749196863
beckham <beckham.lam@mail.mcgill.ca>
2019-03-31 00:34:11 -0400

    two sum fix

--- Easy/Java/twoSum.java
+++ Easy/Java/twoSum.java
@@ -9,7 +9,7 @@ class Solution {
         int[] ans = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.get(target-nums[i]) == null) {
+            if (!map.containsKey(target-nums[i])) {
                 map.put(nums[i], i);
             }
             else {
