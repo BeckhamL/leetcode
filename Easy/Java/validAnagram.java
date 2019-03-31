1ff046763ddd2c6186f7cddbd72a964b85003d90
beckham <beckham.lam@mail.mcgill.ca>
2019-03-31 15:42:40 -0400

    valid anagram fix

--- Easy/Java/validAnagram.java
+++ Easy/Java/validAnagram.java
@@ -1,35 +1,3 @@
-// Very slow solution
-// Add all chars into arraylist, iterate through the first string and remove common chars
-// if arraylist is empty, true else false
-class Solution {
-    public boolean isAnagram(String s, String t) {
-
-        ArrayList<Character> tString = new ArrayList<>();
-
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            tString.add(t.charAt(i));
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            if (tString.contains(s.charAt(i))) {
-                int index = tString.indexOf(s.charAt(i));
-                tString.remove(index);
-            }
-        }
-
-        if (tString.isEmpty()) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-
 // Solution: make both strings into char arrays and sort. Since they are anagrams they should contain the same values. Iterate and if they arent the same return false
 // Runtime: O(n log n)
 
@@ -53,6 +21,41 @@ class Solution {
                 return false;
             }
         }
+        return true;
+    }
+}
+
+// Solution: Go through each string and add the character frequency to the letters array. Loop through both letter arrays and
+// if the values are different, return false.
+// Runtime: O(n)
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+
+        int[] letters1 = new int[26];
+        int[] letters2 = new int[26];
+
+        for (int i = 0; i < arr1.length; i++) {
+            letters1[arr1[i]-'a']++;
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            letters2[arr2[i]-'a']++;
+        }
+
+        for (int i = 0; i < letters1.length; i++) {
+            if (letters1[i] != letters2[i]) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
