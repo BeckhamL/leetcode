70cda10b7d80d57ddb60134385aab9d856958d47
Beckham Lam <beckham.lam@mail.mcgill.ca>
2019-04-03 18:55:39 -0400

    Update longestSubstringWithoutRepeat.java

--- Medium/Java/longestSubstringWithoutRepeat.java
+++ Medium/Java/longestSubstringWithoutRepeat.java
@@ -4,25 +4,29 @@
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        
         int max = 0;
+
         int slow = 0;
         int fast = 0;
-
+        int curr = 0;
+        
         HashSet<Character> seen = new HashSet<>();
-
+        
         while (slow < s.length() && fast < s.length()) {
-
+            
             if (!seen.contains(s.charAt(fast))) {
-
+                
                 seen.add(s.charAt(fast));
                 fast++;
-
-                max = Math.max(max, fast-slow);
+                curr++;
+                
+                max = Math.max(max, curr);
             }
             else {
                 seen.remove(s.charAt(slow));
                 slow++;
+                curr--;
             }
         }
         return max;
