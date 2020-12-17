cc4e1a9cb628902274e89c4f52d9a672aa742066
Beckham Lam <beckham.lam@mail.mcgill.ca>
2020-12-17 16:26:39 -0500

    Create searchInsertPosition.ts

---
+++ Easy/TypeScript/searchInsertPosition.ts
@@ -0,0 +1,24 @@
+function searchInsert(nums: number[], target: number): number {
+
+    let insertIndex = 0;
+    
+    if (nums.length === 0) {
+        return 0;
+    }    
+    
+    // If I find index, return
+    // if the element I look at is bigger, im in right spot since sorted
+    for (let i = 0; i < nums.length; i++) {
+        
+        if (nums[i] === target) {
+            return i;
+        } else if (target < nums[i]) {
+            return i;
+        }
+        
+    }
+    
+    // If those 2 cases fail, it must mean target is largest element
+    return nums.length;
+    
+};
